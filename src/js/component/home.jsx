19ac1b84f73dec43bec0bcf5409ea4e0d2790095
JurBri <89058157+JurBri@users.noreply.github.com>
2024-06-14 02:02:49 +0000

    Se crea el componente con la funcion de agregar y eliminar tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React,{useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { array } from "prop-types";
 
-//create your first component
+
+
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [taskList, setTaskList] = useState([]); 
+
+	const addTask = (value)=>{
+		setTaskList([...taskList, value])
+		setInputValue("")
+	}
+
+	const handleDeleteTask = (index) => {
+		setTaskList(taskList.filter((task, i) => i!== index));
+	  }
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="d-flex flex-column align-items-center" >
+			<div className="todo-list w-25">
+				<div className="form-floating mb-3  ">
+					<input value={inputValue} type="text" class="form-control" id="floatingInput" onChange={(e)=> setInputValue(e.target.value)} onKeyDown={(e)=>{
+						if(e.key == "Enter" && inputValue != ""){
+							addTask(inputValue)
+						}
+					}} />
+					<label for="floatingInput">Your task</label>
+				</div>
+				<ul className="list-group">
+					{taskList && taskList.length > 0 && taskList.map((task, index)=> {
+						return (
+							<li className="list-group-item" key={index}>{task}
+							<button className="btn btn-dark btn-xxs m-2" onClick={() => handleDeleteTask(index)}> <small className=" text-danger">X</small> </button>
+							</li>
+						)
+					})}
+					
+				</ul>
+				<small>{taskList.length+" items left"}</small>
+			</div>
+
+		</div> 
+
 	);
 };
 
