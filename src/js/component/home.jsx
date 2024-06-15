37ea44a346cd7f7dae1cc70a345f0d6f8c999984
JurBri <89058157+JurBri@users.noreply.github.com>
2024-06-15 21:56:38 +0000

    Se arregla los errores del className y hmtlFor, se pone el mensje agregar tareas, y el boton eliminar solo se pone en la tarea q quiero iminar al seleccionar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,42 +8,59 @@ import { array } from "prop-types";
 
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
-	const [taskList, setTaskList] = useState([]); 
+	const [taskList, setTaskList] = useState([]);
+	const [key, setKey] = useState(null);
 
-	const addTask = (value)=>{
+	const addTask = (value) => {
 		setTaskList([...taskList, value])
 		setInputValue("")
 	}
 
 	const handleDeleteTask = (index) => {
-		setTaskList(taskList.filter((task, i) => i!== index));
-	  }
+		setTaskList(taskList.filter((task, i) => i !== index));
+	}
+
 
 	return (
 		<div className="d-flex flex-column align-items-center" >
 			<div className="todo-list w-25">
 				<div className="form-floating mb-3  ">
-					<input value={inputValue} type="text" class="form-control" id="floatingInput" onChange={(e)=> setInputValue(e.target.value)} onKeyDown={(e)=>{
-						if(e.key == "Enter" && inputValue != ""){
+					<input value={inputValue} type="text" className="form-control" id="floatingInput" onChange={(e) => setInputValue(e.target.value)} onKeyDown={(e) => {
+						if (e.key == "Enter" && inputValue != "") {
 							addTask(inputValue)
 						}
 					}} />
-					<label for="floatingInput">Your task</label>
+					<label htmlFor="floatingInput">Your task</label>
 				</div>
 				<ul className="list-group">
-					{taskList && taskList.length > 0 && taskList.map((task, index)=> {
-						return (
-							<li className="list-group-item" key={index}>{task}
-							<button className="btn btn-dark btn-xxs m-2" onClick={() => handleDeleteTask(index)}> <small className=" text-danger">X</small> </button>
-							</li>
+					{
+
+						taskList && taskList.length > 0 ? taskList.map((task, index) => {
+							return (
+								<li onMouseEnter={() => setKey(index)}
+									onMouseLeave={() => setKey(null)} className="list-group-item" key={index}>{task}
+									{
+										key === index && // me muestra el boton 
+										<button
+											className="btn btn-dark btn-xxs m-2"
+											onClick={() => handleDeleteTask(index)}> <small className=" text-danger">X</small>
+										</button>
+									}
+								</li>
+							)
+						}) : (
+							<h3>
+								Agrega tus tareas
+							</h3>
 						)
-					})}
-					
+
+					}
+
 				</ul>
-				<small>{taskList.length+" items left"}</small>
+				<small>{taskList.length + " items left"}</small>
 			</div>
 
-		</div> 
+		</div>
 
 	);
 };
